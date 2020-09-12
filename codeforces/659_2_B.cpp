8779831e15edba71e80fcbbf70adc7b66afeecae
askeySnip <lz.askey@gmail.com>
2020-09-12 21:16:30 +0800

    codeforces

---
+++ codeforces/659_2_B.cpp
@@ -0,0 +1,114 @@
+/*
+ID: leezhen
+TASK: practice
+LANG: C++11
+*/
+#include <assert.h>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <new>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<pair<int, int> > vii;
+typedef long long ll;
+#define bit(x, i) (x & (1 << i))
+#define in(i, l, r) (l < i && i < r)
+#define linr(i, l, r) (l <= i && i <= r)
+#define lin(i, l, r) (l <= i && i < r)
+#define inr(i, l, r) (l < i && i <= r)
+#define gi(a) scanf("%d", &a)
+#define gii(a, b) scanf("%d%d", &a, &b)
+#define giii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define gs(x) scanf("%s", x)
+#define clr(a, x) memset(a, x, sizeof(a))
+#define c2i(c) (c - '0')
+#define sz(x) ((int)((x).size()))
+#define all(c) (c).begin(), (c).end()
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+// for debug
+#define what_is(x) \
+  cerr << "Line " << __LINE__ << ": " << #x << " is " << (x) << endl;
+#define error(args...)                       \
+  {                                          \
+    string _s = #args;                       \
+    replace(_s.begin(), _s.end(), ',', ' '); \
+    stringstream _ss(_s);                    \
+    istream_iterator<string> _it(_ss);       \
+    err(_it, args);                          \
+  }
+void err(istream_iterator<string> it) { cerr << endl; }
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+  cerr << *it << " = " << a << endl;
+  err(++it, args...);
+}
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); i++)
+const int inf = 0x3f3f3f3f;
+const int mod = 1e9 + 7;
+const int fx[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int fxx[8][2] = {{0, 1}, {0, -1}, {1, 0},  {-1, 0},
+                       {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+// struct
+
+// data
+int n, k, l;
+vi d;
+
+bool solve() {
+  REP(i, 1, n + 1) if (d[i] > l) return false;
+  vi safe;
+  safe.push_back(0);
+  REP(i, 1, n + 1) if (d[i] + k <= l) safe.push_back(i);
+  safe.push_back(n + 1);
+  REP(i, 1, sz(safe)) {
+    int tide = k;
+    bool down = true;
+    REP(j, safe[i - 1] + 1, safe[i]) {
+      tide += down ? -1 : +1;
+      if (down) tide -= max(0, d[j] + tide - l);
+      if (tide < 0 || d[j] + tide > l) return false;
+      if (tide == 0) down = false;
+    }
+  }
+  return true;
+}
+
+int main() {
+  int t;
+  gi(t);
+  while (t--) {
+    giii(n, k, l);
+    d.resize(n + 2);
+    REP(i, 1, n + 1) gi(d[i]);
+    bool t = solve();
+    if (t)
+      printf("Yes\n");
+    else
+      printf("No\n");
+  }
+  return 0;
+}
---
+++ codeforces/659_2_C.cpp
@@ -0,0 +1,119 @@
+/*
+ID: leezhen
+TASK: practice
+LANG: C++11
+*/
+#include <assert.h>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <new>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<pair<int, int> > vii;
+typedef long long ll;
+#define bit(x, i) (x & (1 << i))
+#define in(i, l, r) (l < i && i < r)
+#define linr(i, l, r) (l <= i && i <= r)
+#define lin(i, l, r) (l <= i && i < r)
+#define inr(i, l, r) (l < i && i <= r)
+#define gi(a) scanf("%d", &a)
+#define gii(a, b) scanf("%d%d", &a, &b)
+#define giii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define gs(x) scanf("%s", x)
+#define clr(a, x) memset(a, x, sizeof(a))
+#define c2i(c) (c - '0')
+#define sz(x) ((int)((x).size()))
+#define all(c) (c).begin(), (c).end()
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+// for debug
+#define what_is(x) \
+  cerr << "Line " << __LINE__ << ": " << #x << " is " << (x) << endl;
+#define error(args...)                       \
+  {                                          \
+    string _s = #args;                       \
+    replace(_s.begin(), _s.end(), ',', ' '); \
+    stringstream _ss(_s);                    \
+    istream_iterator<string> _it(_ss);       \
+    err(_it, args);                          \
+  }
+void err(istream_iterator<string> it) { cerr << endl; }
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+  cerr << *it << " = " << a << endl;
+  err(++it, args...);
+}
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); i++)
+const int inf = 0x3f3f3f3f;
+const int mod = 1e9 + 7;
+const int fx[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int fxx[8][2] = {{0, 1}, {0, -1}, {1, 0},  {-1, 0},
+                       {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+// struct
+
+// data
+const int maxn = 1e5 + 10;
+char a[maxn], b[maxn];
+vi adj[20];
+int mark[20];
+
+void dfs(int u) {
+  mark[u] = 1;
+  for (auto v : adj[u]) {
+    if (!mark[v]) dfs(v);
+  }
+}
+
+int main() {
+  int t;
+  gi(t);
+  while (t--) {
+    int n;
+    gi(n);
+    gs(a);
+    gs(b);
+    bool ok = true;
+    REP(i, 0, 20) adj[i].clear();
+    REP(i, 0, n) {
+      if (a[i] > b[i]) {
+        ok = false;
+        printf("-1\n");
+        break;
+      } else {
+        adj[a[i] - 'a'].push_back(b[i] - 'a');
+        adj[b[i] - 'a'].push_back(a[i] - 'a');
+      }
+    }
+    if (!ok) continue;
+    memset(mark, 0, sizeof mark);
+    int ans = 20;
+    REP(i, 0, 20) {
+      if (!mark[i]) dfs(i), --ans;
+    }
+    printf("%d\n", ans);
+  }
+  return 0;
+}
